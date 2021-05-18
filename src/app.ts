17bf11f7398bfe6c3fd930b88858c397dc747638
demola malomo <malomo.alaba@yahoo.com>
2021-05-18 09:48:54 +0100

    database, logger and app setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log(123456);
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db';
+import morgan from 'morgan';
+
+dotenv.config();
+
+//database and app configuration;
+connectDB();
+const app = express();
+app.use(morgan('combined'));
+
+const PORT = process.env.PORT || 6000;
+
+//middlewares
+app.use(express.json());
+
+app.listen(PORT, () => {
+	console.log(`Server listening on PORT: ${PORT}`);
+});
